3b634177755e4f39708d22b26128dffc501f8db0
docr-ROI-diagostic-ecuacion-KAI <doctor.roi.marketing@gmail.com>
2026-06-12 22:56:31 +0200

    Load phase-specific guidance

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App2 } from "./App2";
 import "./styles.css";
+import "./phase-guidance";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
