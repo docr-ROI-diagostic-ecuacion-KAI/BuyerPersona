c084ad1fccc479d647b30433caa82866410516b6
docr-ROI-diagostic-ecuacion-KAI <doctor.roi.marketing@gmail.com>
2026-06-14 12:38:02 +0200

    Load empathy map replacement

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App2 } from "./App2";
 import "./styles.css";
 import "./phase-guidance";
+import "./empathy-map-replacement";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
