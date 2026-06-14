ff64289c057441e43ed1579fd5ad6ee8534513be
docr-ROI-diagostic-ecuacion-KAI <doctor.roi.marketing@gmail.com>
2026-06-14 11:49:28 +0200

    Remove legacy empathy canvas injection

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { App2 } from "./App2";
 import "./styles.css";
 import "./phase-guidance";
-import "./empathy-canvas";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
